e5c558516b27128b61a471e35315971834bbd8f3
Amin Mousavi <amn.mousavi@gmail.com>
2023-03-10 21:34:54 +0330

    Add app colors

--- app/src/main/kotlin/com/aminmsvi/baghche/MainActivity.kt
+++ app/src/main/kotlin/com/aminmsvi/baghche/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.aminmsvi.baghche.ui.theme.baghcheTheme
+import com.aminmsvi.baghche.ui.theme.BaghcheTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            baghcheTheme {
+            BaghcheTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -36,7 +36,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    baghcheTheme {
+    BaghcheTheme {
         Greeting("Android")
     }
 }
